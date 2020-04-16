9e04488f3334b7754e3e8656f17e0b8ab09bd59f
GasaGentille <gisagentille55@gmail.com>
2020-04-16 16:17:10 +0200

    first commit

---
+++ run.py
@@ -0,0 +1,8 @@
+from app import app
+
+# if __name__ == '__main__':
+#     app.run(debug = True)
+
+# WE REMOVE THE debug = True because the debug mode has been enabled in the congifuration file
+if __name__ == '__main__':
+    app.run()
